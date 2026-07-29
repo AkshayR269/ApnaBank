b1aa3eabddbd6ffa49501cfd83a3319b426ee813
Akshay269 <aksrahangdale@gmail.com>
2026-07-29 15:17:02 +0530

    project folders initialized

---
+++ src/main/java/apexbank_backend/ApnabankBackendApplication.java
@@ -0,0 +1,13 @@
+package apexbank_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApnabankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApnabankBackendApplication.class, args);
+	}
+
+}
